b56e540e68d7a99304494230d66cae66fb9e8802
gayatri-p786 <78681507+GayatriVP@users.noreply.github.com>
2024-02-10 12:06:28 -0800

    flask basic fetch done

--- HW-2/main.py
+++ HW-2/main.py
@@ -1,29 +1,37 @@
-import datetime
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/")
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [
-        datetime.datetime(2018, 1, 1, 10, 0, 0),
-        datetime.datetime(2018, 1, 2, 10, 30, 0),
-        datetime.datetime(2018, 1, 3, 11, 0, 0),
-    ]
+#API KEY = cmuu051r01qru65i12s0cmuu051r01qru65i12sg
 
-    return render_template("index.html", times=dummy_times)
+@app.route('/search_stock', methods=['GET'])
+def search_stock():
+    stock_ticker = request.args.get('stock_ticker')
+    
+    if not stock_ticker:
+        return jsonify({'error': 'Stock ticker is required'}), 400
 
+    # Make a request to the Finnhub Stock API
+    finnhub_api_key = 'cmuu051r01qru65i12s0cmuu051r01qru65i12sg'
+    endpoint = f'https://finnhub.io/api/v1/stock/profile2?symbol={stock_ticker}&token={finnhub_api_key}'
+    
+    try:
+        print(endpoint)
+        response = requests.get(endpoint)
+        data = response.json()
+        print(data)
+        # Return the API response to the client
+        # return jsonify({'data':data}),200
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': f'Error fetching data from Finnhub API: {str(e)}'}), 500
 
-if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host="127.0.0.1", port=8080, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
--- HW-2/static/script.js
+++ HW-2/static/script.js
@@ -1,7 +1,43 @@
-'use strict';
+// main.js
 
-window.addEventListener('load', function () {
-
-  console.log("Hello World!");
-
+ // Add an event listener to the form
+ document.getElementById('stockSearchForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+  searchStock(); // Call your searchStock function
 });
+
+
+function displayErrorMessage(message) {
+  var errorSection = document.getElementById('errorSection');
+  errorSection.style.display = 'block';
+  // errorSection.style.backgroundColor = '#dddddd'; // Gray background
+  errorSection.innerHTML = message;
+}
+
+function searchStock() {
+  var stockTicker = document.getElementById('stockTicker').value;
+  fetch(`/search_stock?stock_ticker=${stockTicker}`,{method:'GET'})
+    .then(response => response.json())
+    .then(data => {
+      if (Object.keys(data).length === 0) {
+        // Empty JSON object, display error message
+        displayErrorMessage('Error: No record has been found, please enter a valid symbol');
+    } 
+    else{
+
+    
+      console.log("in data")
+      document.getElementById('searchResult').innerHTML = JSON.stringify(data,null,2);
+    }
+  })
+    .catch(error => console.error(error));
+}
+
+
+
+function clearResult() {
+  document.getElementById('stockTicker').value = '';
+  document.getElementById('searchResult').innerHTML = '';
+  errorSection.innerHTML = '';
+  errorSection.style.display = 'none';
+}
