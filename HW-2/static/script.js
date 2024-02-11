dc5e41f6cd7d47a84de9b5938b5666773213be57
gayatri-p786 <78681507+GayatriVP@users.noreply.github.com>
2024-02-10 16:05:00 -0800

    company tab done

--- HW-2/static/script.js
+++ HW-2/static/script.js
@@ -24,10 +24,11 @@ function searchStock() {
         displayErrorMessage('Error: No record has been found, please enter a valid symbol');
     } 
     else{
-
+      activateTab('companyTab', 'companyContent');
+      displayCompanyData(data);
     
-      console.log("in data")
-      document.getElementById('searchResult').innerHTML = JSON.stringify(data,null,2);
+      // console.log("in data")
+      // document.getElementById('searchResult').innerHTML = JSON.stringify(data,null,2);
     }
   })
     .catch(error => console.error(error));
@@ -41,3 +42,53 @@ function clearResult() {
   errorSection.innerHTML = '';
   errorSection.style.display = 'none';
 }
+
+
+
+// Function to activate tab and corresponding content
+function activateTab(tabId, contentId) {
+  // Deactivate all tabs and hide content
+  document.querySelectorAll('.tab').forEach(tab => {
+      tab.classList.remove('active');
+  });
+  document.querySelectorAll('.tab-content').forEach(content => {
+      content.classList.remove('active');
+  });
+  // Activate clicked tab and show corresponding content
+  document.getElementById(tabId).classList.add('active');
+  document.getElementById(contentId).classList.add('active');
+}
+
+  // Function to display company data
+  function displayCompanyData(data) {
+    const companyContent = document.getElementById('companyContent');
+    companyContent.innerHTML = `
+    <div class="company-info">
+        <img src="${data.logo}" alt="Company Logo">
+        <table>
+            <tr>
+                <td>Company Name</td>
+                <td>${data.name}</td>
+            </tr>
+            <tr>
+                <td>Stock Ticker Symbol</td>
+                <td>${data.ticker}</td>
+            </tr>
+            <tr>
+                <td>Stock Exchange Code</td>
+                <td>${data.exchange}</td>
+            </tr>
+            <tr>
+                <td>Company IPO Date</td>
+                <td>${data.ipo}</td>
+            </tr>
+            <tr>
+                <td>Category</td>
+                <td>${data.finnhubIndustry}</td>
+            </tr>
+        </table>
+      </div>
+    `;
+}
+
+
